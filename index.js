5ba8d6d22d96468bacaaf8876b8fd12c2762c063
shemaeric <shemaeric01@gmail.com>
2019-04-03 22:45:16 +0200

    [finish #165020195] refactor the index

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import express from 'express';
-<<<<<<< HEAD
-=======
-import { resolve } from 'dns';
-import { rejects } from 'assert';
->>>>>>> [start #165020195] set up babel for transpiling es6 to es65
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-<<<<<<< HEAD
 
 // setting up the root enpoint for the testing
 app.get('/', (req, res) => {
@@ -17,18 +11,9 @@ app.get('/', (req, res) => {
 })
 
 // setting the port to run at 3000 if there is no other port set on the environment
-=======
-
-app.get('/', (req, res) => {
-    res.status(200).send({message: 'Welcome to Authors Haven'})
-})
->>>>>>> [start #165020195] set up babel for transpiling es6 to es65
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App running at PORT ${PORT}`));
 
-<<<<<<< HEAD
 // export the app to access the port into the test files
-=======
->>>>>>> [start #165020195] set up babel for transpiling es6 to es65
 export default app;
