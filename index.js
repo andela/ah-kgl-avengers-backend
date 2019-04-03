44fd1f399f6b2d882de70e59a2f6d3ac26afd8ea
shemaeric <shemaeric01@gmail.com>
2019-04-03 22:45:16 +0200

    [finish #165020195] refactor the index

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from 'express';
-import { resolve } from 'dns';
-import { rejects } from 'assert';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// setting up the root enpoint for the testing
 app.get('/', (req, res) => {
     res.status(200).send({message: 'Welcome to Authors Haven'})
 })
+
+// setting the port to run at 3000 if there is no other port set on the environment
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App running at PORT ${PORT}`));
 
+// export the app to access the port into the test files
 export default app;
