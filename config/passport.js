9fda520b60633939f03a6ec4064ffe06c83caa4b
avpaul <vabimana@gmail.com>
2019-04-08 10:31:54 +0200

    [ch 165020191] Setup Passport fro user auth

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,28 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+import passport from "passport";
+import strategies from "passport-local";
+import models from '../models/';
 
+const LocalStrategy = strategies.Strategy;
+const User = models.User;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "user[email]",
+      passwordField: "user[password]"
+    },
+    function(email, password, done) {
+      User.findOne({ email: email })
+        .then(function(user) {
+          if (!user || !user.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
+
+          return done(null, user);
+        })
+        .catch(done);
+    }
+  )
+);
