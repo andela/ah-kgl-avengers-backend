1bf427f8c71d9ae7d717df4e1b2970a830f46900
Shema Eric <36806576+shemaeric@users.noreply.github.com>
2019-04-08 09:57:12 +0200

    #165020195- Setup tests (#4)

---
+++ tests/index.test.js
@@ -0,0 +1,30 @@
+import chai from 'chai';
+import chaitHttp from 'chai-http';
+import app from '../index';
+
+const should = chai.should();
+
+chai.use(chaitHttp);
+
+// testing the root rout
+describe('Root test', () => {
+    it('should return a response with status code 200 and JSON object', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a('object');
+                done();
+            });
+    });
+
+    it('should not pass as the URL provided is not valid and returns error object', (done) => {
+        chai.request(app)
+            .get('/ddddd')
+            .end((err, res) => {
+                res.should.have.status(404);
+                res.should.be.a('object');
+                done();
+            });
+    });
+})
