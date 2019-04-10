824cc6b45495864647ad99bf69a85c72b29a8f6a
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-04-10 21:20:33 +0200

    [Feature #165020201] Creating dynamic template using handlebars

--- config/verificationMail.js
+++ config/verificationMail.js
@@ -1,34 +1,51 @@
-const nodemailer = require('nodemailer');
-const ENV = require('dotenv');
+import nodemailer from 'nodemailer';
+import Handlebars from 'handlebars';
+import fs from 'fs';
+import path from 'path';
+import ENV from 'dotenv';
 
 ENV.config();
 
-async function sentActivationMail(params) {
+const config = {
+  /**
+   * Function to send the activation mail to the user.
+   * below params are all required to successfully send the email
+   *
+   * @param { name, id, email } params
+   */
 
-  var client = nodemailer.createTransport({
-    service: process.env.TRANSPORT_SERVICE,
-    auth: {
-      user: process.env.TRANSPORT_USER,
-      pass: process.env.TRANSPORT_PASS,
+  async sentActivationMail(params) {
+    const client = nodemailer.createTransport({
+      service: process.env.TRANSPORT_SERVICE,
+      auth: {
+        user: process.env.TRANSPORT_USER,
+        pass: process.env.TRANSPORT_PASS,
+      }
+    });
+
+    const source = fs.readFileSync(path.join(__dirname, '../template/activation.hjs'), 'utf8');
+    const template = Handlebars.compile(source);
+
+    const {
+      name, id, email,
+    } = params;
+
+    const url = `http://localhost:3000/api/v1/activation/${id}`;
+    const envelope = {
+      from: process.env.SENDER,
+      to: email,
+      subject: 'Account activation',
+      html: template({ name, url }),
+    };
+    try {
+      return await client.sendMail(envelope);
+    } catch (error) {
+      return {
+        status: 500,
+        message: 'Failed to send the email',
+      };
     }
-  });
+  },
+};
 
-  const {
-    username,
-    email,
-    file
-  } = params;
-
-  const envelope = {
-    from: '"Kigali Avengers" <info@kgl-avengers.com>',
-    to: email,
-    subject: 'Account activation ',
-    html: file,
-  };
-  try {
-    const info = await client.sendMail(envelope);
-    console.log(info);
-  } catch (error) {
-    return error;
-  }
-}
+export default config;
