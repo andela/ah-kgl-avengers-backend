1df64385991f94af5370c41a2823ec32fa145b6f
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-04-11 22:09:14 +0200

    [Feature #165020201] Add activation route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,37 @@
-import express from 'express';
-import passport from 'passport';
-import userControllers from '../../controllers/users';
+import express from "express";
+import passport from "passport";
+import userControllers from "../../controllers/users";
 
 const router = express.Router();
 
-router.post('/auth/signup', userControllers.createUserLocal);
-router.post('/auth/login', userControllers.signinLocal);
-router.get('/activation/:id', userControllers.activateUserAccount);
+router.post("/auth/signup", userControllers.createUserLocal);
+router.post("/auth/login", userControllers.signinLocal);
+router.get("/activation/:id", userControllers.activateUserAccount);
 
 // for testing the passport authentication of the JWT token
-router.get('/test',
-  passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.send({ message: 'hello' });
-  });
+router.get(
+  "/test",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.send({ message: "hello" });
+  }
+);
 
 // Facebook Authentication Routes
-router.post('/oauth/facebook', passport.authenticate('facebookOAuth',
-  { session: false }), userControllers.createUserSocial);
+router.post(
+  "/oauth/facebook",
+  passport.authenticate("facebookOAuth", { session: false }),
+  userControllers.createUserSocial
+);
 
-router.post('/oauth/google', passport.authenticate('googleOAuth',
-  { session: false }), userControllers.createUserSocial);
+router.post(
+  "/oauth/google",
+  passport.authenticate("googleOAuth", { session: false }),
+  userControllers.createUserSocial
+);
 
 // Reset password
-router.post('/users/reset', userControllers.resetPassword);
-router.put('/users/reset/:token', userControllers.updatePassword);
-
+router.post("/users/reset", userControllers.resetPassword);
+router.put("/users/reset/:token", userControllers.updatePassword);
 
 export default router;
