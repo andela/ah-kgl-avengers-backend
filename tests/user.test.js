ca86e5f1d949729ebc4b178309bc8bc2bfe09ad0
shemaeric <shemaeric01@gmail.com>
2019-04-11 18:42:17 +0200

    apply the review feedbacks

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import dotenv from 'dotenv';
@@ -16,30 +15,15 @@ chai.use(chaiHttp);
 const googleToken = process.env.GOOGLE_TOKEN;
 const facebookToken = process.env.FACEBOOK_TOKEN;
 
-// testing facebook and google strategy
-describe('Social login routes', () => {
-=======
-
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../index';
-import models from '../models';
-
-// importing the model to access the User Object
-const { User } = models;
-
-chai.use(chaiHttp);
 
 describe('User', () => {
   // delete all datas in the table of users before doing tests
->>>>>>> [finishes #165020200] refactor and fix the tests
   before(() => {
     User.destroy({
       where: {},
       truncate: true
     });
   });
-<<<<<<< HEAD
 
   it('should return an object with status 200 when a user login with Google OAuth', (done) => {
     chai.request(app)
@@ -59,10 +43,11 @@ describe('User', () => {
         res.body.should.be.a('object');
         done();
       });
-=======
+  });
+
   describe('/POST User Signup', () => {
     it('should pass and returs the status:200 as the user provides all required datas for login', (done) => {
-      const newUser = { username: 'berry', email: 'check@tests.com', password: 'test' };
+      const newUser = { username: 'berra', email: 'checka@tests.com', password: 'test' };
       chai.request(app)
         .post('/api/v1/auth/signup')
         .send(newUser)
@@ -73,8 +58,8 @@ describe('User', () => {
         });
     });
 
-    it('should not pass and returns status:400 as the user is already in db', (done) => {
-      const newUser = { username: 'berry', email: 'check@tests.com', password: 'test' };
+    it('should pass and returns status:400 as the user is already in db', (done) => {
+      const newUser = { username: 'berra', email: 'checka@tests.com', password: 'test' };
       chai.request(app)
         .post('/api/v1/auth/signup')
         .send(newUser)
@@ -87,7 +72,7 @@ describe('User', () => {
 
   describe('/POST Signin', () => {
     it('should pass and returns the status:200 and the object with token', (done) => {
-      const signUser = { email: 'check@tests.com', password: 'test' };
+      const signUser = { email: 'checka@tests.com', password: 'test' };
       chai.request(app)
         .post('/api/v1/auth/login')
         .send(signUser)
@@ -97,8 +82,8 @@ describe('User', () => {
           done();
         });
     });
-    it('should not pass and returns the error object and status:400 as password doen not match', (done) => {
-      const signUser = { email: 'check@tests.com', password: 'tesst' };
+    it('should pass and returns the error object and status:400 as password doen not match', (done) => {
+      const signUser = { email: 'checka@tests.com', password: 'tesst' };
       chai.request(app)
         .post('/api/v1/auth/login')
         .send(signUser)
@@ -108,6 +93,25 @@ describe('User', () => {
           done();
         });
     });
->>>>>>> [finishes #165020200] refactor and fix the tests
+  });
+
+  describe('/USER object in models', () => {
+    it('should pass and returns the User object from the db that was created', async () => {
+      await User.create({ username: 'berria', email: 'checky@tests.com', hash: 'test' })
+        .then((res) => {
+          res.should.be.a('object');
+        })
+        .catch(err => console.log(err));
+    });
+
+    it('should pass and returns the error array', async () => {
+      await User.create({ username: 'berria', email: 'checky@tests.com', password: 'test' })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          err.errors.should.be.a('array');
+        });
+    });
   });
 });
