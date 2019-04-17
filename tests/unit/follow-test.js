993daea7b2d91b07466294c3718a93f99f28069e
avpaul <vabimana@gmail.com>
2019-04-17 21:25:19 +0200

    [ft #165020213] Finish user follow and unfollow

---
+++ tests/unit/follow-test.js
@@ -0,0 +1,27 @@
+// import chai from 'chai';
+// import sinon from 'sinon';
+// import User from '../../controllers/users';
+//
+// chai.should();
+//
+// const req = {
+//   body: {}
+// };
+// const res = {};
+// const next = () => {};
+//
+// describe('Follow', () => {
+//   it('should return a 201 status code and profile of the followed user', () => {
+//     const response = User.follow(req, res, next);
+//     response.should.have.status(201);
+//     response.body.profile.should.be.an('object');
+//   });
+// });
+//
+// describe('Un-follow', () => {
+//   it('should return a 200 status code and profile of the un-followed user', () => {
+//     const response = User.follow(req, res, next);
+//     response.should.have.status(200);
+//     response.body.profile.should.be.an('object');
+//   });
+// });
