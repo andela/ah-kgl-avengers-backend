ffd9141873cc369b460c387dedaa6c47db384eaf
avpaul <vabimana@gmail.com>
2019-04-18 18:06:20 +0200

    [ft #165020213] Change test database environment name

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     logging: false
   },
   test: {
-    use_env_variable: 'TEST_DATABASE_URL',
+    use_env_variable: 'DATABASE_TEST',
     dialect: 'postgres',
     logging: false,
   },
