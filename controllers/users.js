a0ac2e4700534c4c4208bbd9ee948048a60a6bd1
shemaeric <shemaeric01@gmail.com>
2019-06-17 14:06:46 +0200

    ch(change URL): change the redirect url - change the redirect URL after the user activates his/her account
    [Delivers #166727829]:

--- controllers/users.js
+++ controllers/users.js
@@ -70,11 +70,8 @@ class Users {
   static async activateUserAccount(req, res) {
     const { token } = req.params;
     const { email } = jwt.verify(token, process.env.SECRET);
-    await User.update({ activated: 1 }, { where: { email }, returning: true });
-    return res.status(201).send({
-      status: res.statusCode,
-      message: 'Your account updated successfully'
-    });
+    const data = await User.update({ activated: 1 }, { where: { email }, returning: true });
+    if (data[0] === 1) res.redirect(`${process.env.FRONT_END_URL}/login`);
   }
 
   /**
